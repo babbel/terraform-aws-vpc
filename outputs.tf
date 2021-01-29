15ddf9511532a512c99b8b6a25105a039674733e
Jan Sebastian Siwy <git@jansiwy.de>
2021-01-29 11:37:30 +0100

    add outputs for subnet groups

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-output "this" {
-  description = "VPC"
-  value       = aws_vpc.this
+output "db_subnet_group" {
+  description = "DB subnet group containing all private subnets"
+  value = var.create_db_subnet_group ? aws_db_subnet_group.this : null
+}
+
+output "db_elasticache_subnet_group" {
+  description = "ElastiCache subnet group containing all private subnets"
+  value = var.create_db_elasticache_subnet_group ? aws_db_elasticache_subnet_group.this : null
 }
 
 output "private_subnets" {
@@ -11,4 +16,9 @@ output "private_subnets" {
 output "public_subnets" {
   description = "Public subnets"
   value       = module.availability_zone[*].public_subnet
+}
+
+output "this" {
+  description = "VPC"
+  value       = aws_vpc.this
 }
