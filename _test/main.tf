05c554a55706212661265fe0afec700fd0a6a726
Jan Sebastian Siwy <git@jansiwy.de>
2022-06-09 15:07:49 +0000

    Run validation with multiple Terraform versions
    It's always the currently latest patch version for a minor version.

---
+++ _test/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "local"
+}
+
+module "vpc" {
+  source = "./.."
+
+  name       = "example"
+  cidr_block = "10.0.0.0/16"
+  size       = 2
+
+  vpc_endpoints = {
+    gateway   = ["dynamodb", "s3"]
+    interface = ["logs"]
+  }
+
+  tags = {
+    app = "example"
+    env = "production"
+  }  
+}
