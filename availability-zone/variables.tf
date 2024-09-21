b7ba36e445711e9e602895e91c768efed7d25123
Jan Sebastian Siwy <github@jansiwy.de>
2024-09-21 19:58:33 +0200

    Add support for resource-specific tags

--- availability-zone/variables.tf
+++ availability-zone/variables.tf
@@ -1,45 +1,106 @@
-variable "internet_gateway" {
-  description = "Internet Gateway which belongs to `var.vpc`"
+variable "default_tags" {
+  type    = map(string)
+  default = {}
 
+  description = <<EOS
+Map of tags assigned to all AWS resources created by this module.
+EOS
+}
+
+variable "internet_gateway" {
   type = object({
     id = string
   })
+
+  description = <<EOS
+Internet Gateway which belongs to `var.vpc`
+EOS
+}
+
+variable "nat_gateway_eip_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the NAT Gateway EIP.
+EOS
+}
+
+variable "nat_gateway_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the NAT Gateway
+EOS
+}
+
+variable "private_route_table_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the private route table.
+EOS
+}
+
+variable "private_subnet_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the private subnet.
+EOS
+}
+
+variable "public_route_table_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the public route table.
+EOS
+}
+
+variable "public_subnet_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the public subnet.
+EOS
 }
 
 variable "subnet_bits" {
+  type = number
+
   description = <<EOS
-Number of bits to add to the VPC CIDR to get the size of the subnet CIDR
+Number of bits to add to the VPC CIDR to get the size of the subnet CIDR.
 
 This will be the `newbits` argument of [`cidrsubnet`](https://www.terraform.io/docs/language/functions/cidrsubnet.html).
 EOS
-
-  type = number
 }
 
 variable "subnet_index" {
+  type = number
+
   description = <<EOS
-The number of the subnet which will be used to calculate the subnet CIDR
+The number of the subnet which will be used to calculate the subnet CIDR.
 
 This will be used in the `netnum` argument of [`cidrsubnet`](https://www.terraform.io/docs/language/functions/cidrsubnet.html):
 
 * for the public subnet, it will be `2 * var.subnet_index`,
 * for the private subnet, it will be `2 * var.subnet_index + 1`.
 EOS
-
-  type = number
-}
-
-variable "tags" {
-  description = "Map of tags to assign to all resources supporting tags (in addition to the `Name` tag)"
-
-  type = map(string)
 }
 
 variable "vpc" {
-  description = "VPC in which the subnets and routing tables will be created"
-
   type = object({
     id         = string
     cidr_block = string
   })
+
+  description = <<EOS
+VPC in which the subnets and routing tables will be created.
+EOS
 }
--- variables.tf
+++ variables.tf
@@ -1,62 +1,166 @@
 variable "cidr_block" {
-  description = "VPC CIDR"
-
   type = string
+
+  description = <<EOS
+VPC CIDR.
+EOS
 }
 
 variable "create_db_subnet_group" {
-  description = "Whether to create a DB subnet group"
-
   type    = bool
   default = false
+
+  description = <<EOS
+Whether to create a DB subnet group.
+EOS
 }
 
 variable "create_elasticache_subnet_group" {
-  description = "Whether to create an ElastiCache subnet group"
-
   type    = bool
   default = false
+
+  description = <<EOS
+Whether to create an ElastiCache subnet group.
+EOS
+}
+
+variable "db_subnet_group_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the DB subnet group.
+EOS
+}
+
+variable "default_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to all AWS resources created by this module.
+EOS
+}
+
+variable "gateway_vpc_endpoint_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the VPC Endpoints of type `Gateway`.
+EOS
+}
+
+variable "interface_vpc_endpoint_security_group_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the security groups for the VPC Endpoint of type `Interface`.
+EOS
+}
+
+variable "interface_vpc_endpoint_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the VPC Endpoints of type `Interface`.
+EOS
+}
+
+variable "internet_gateway_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the Internet Gateway.
+EOS
 }
 
 variable "name" {
-  description = "Name to be used in all `Name` tags shown in the AWS Console"
-
   type = string
+
+  description = <<EOS
+Name to be used in all `Name` tags shown in the AWS Console.
+EOS
+}
+
+variable "nat_gateway_eip_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the NAT Gateway EIPs.
+EOS
+}
+
+variable "nat_gateway_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the NAT Gateways.
+EOS
+}
+
+variable "private_route_table_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the private route tables.
+EOS
+}
+
+variable "private_subnet_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the private subnets.
+EOS
+}
+
+variable "public_route_table_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the public route tables.
+EOS
+}
+
+variable "public_subnet_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the public subnets.
+EOS
 }
 
 variable "size" {
+  type = number
+
   description = <<EOS
-Number of availability zones to cover
+Number of availability zones to cover.
 
 This will be also the number public-private subnet pairs to create.
 If subnet groups for RDS and/or ElastiCache shall be created, the number must be at least 2.
 EOS
-
-  type = number
 }
 
 variable "subnet_bits" {
-  description = "Number of bits to add to the VPC CIDR to get the size of the subnet CIDR"
-
   type    = number
   default = 4
-}
 
-variable "tags" {
-  description = "Map of tags to assign to all resources supporting tags (in addition to the `Name` tag)"
-
-  type    = map(string)
-  default = {}
+  description = <<EOS
+Number of bits to add to the VPC CIDR to get the size of the subnet CIDR.
+EOS
 }
 
 variable "vpc_endpoints" {
-  description = <<EOS
-Service name identifiers for the VPC endpoints.
-
-Every VPC endpoint belongs to a service name like `com.amazonaws.REGION.IDENTIFIER`.
-The lists of this variable (grouped by VPC endpoint type) are expecting just the `IDENTIFIER` of the service name.
-EOS
-
   type = object({
     gateway   = list(string)
     interface = list(string)
@@ -65,4 +169,20 @@ EOS
     gateway   = []
     interface = []
   }
+
+  description = <<EOS
+Service name identifiers for the VPC endpoints.
+
+Every VPC endpoint belongs to a service name like `com.amazonaws.REGION.IDENTIFIER`.
+The lists of this variable (grouped by VPC endpoint type) are expecting just the `IDENTIFIER` of the service name.
+EOS
+}
+
+variable "vpc_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the VPC.
+EOS
 }
